51a7be7d9eff10f514d6449feca7ce891ed22a34
mgrinko <glukym@gmail.com>
2017-07-20 22:07:25 +0300

    phone click is handled

--- frontend/components/phone-catalogue/phone-catalogue.js
+++ frontend/components/phone-catalogue/phone-catalogue.js
@@ -3,17 +3,30 @@
 import compiledTemplate from './template.hbs';
 import './style.css';
 
+
 export default class PhoneCatalogue {
   constructor(options) {
     this._el = options.el;
     this._phones = options.phones;
 
     this._render();
+
+    this._el.addEventListener('click', this._onPhoneClick.bind(this));
   }
 
   _render() {
     this._el.innerHTML = compiledTemplate({
       phones: this._phones
     });
+  }
+
+  _onPhoneClick(event) {
+    let phoneElement = event.target.closest('[data-element="phone"]');
+
+    if (phoneElement) {
+      return;
+    }
+
+    console.log(phoneElement.dataset.phoneId);
   }
 }
